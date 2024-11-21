27acd848cf55e693e129cb651d9a7ea2c25a7276
Mitesh-Chaudhari44 <miteshchaudhari4466@gmail.com>
2024-11-21 11:24:31 +0530

    Updated

--- Audi symbol.cpp
+++ Audi symbol.cpp
@@ -1,124 +1,59 @@
-// Bresenham house
 #include <GL/glut.h>
-int x1, x2, y1, y2;
-void myInit()
+void init()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glColor3f(1.0, 1.0, 1.0);
     gluOrtho2D(0, 500, 0, 500);
 }
-void draw_pixel(int x, int y)
+void plot(int x, int y, int xc, int yc)
 {
     glBegin(GL_POINTS);
-    glVertex2i(x, y);
+    glVertex2i(xc + x, yc + y);
+    glVertex2i(xc - x, yc + y);
+    glVertex2i(xc + x, yc - y);
+    glVertex2i(xc - x, yc - y);
+    glVertex2i(xc + y, yc + x);
+    glVertex2i(xc - y, yc + x);
+    glVertex2i(xc + y, yc - x);
+    glVertex2i(xc - y, yc - x);
     glEnd();
 }
-void draw_line(int x1, int x2, int y1, int y2)
+void bresenhamCircle(int xc, int yc, int r)
 {
-    int dx, dy, i, e;
-    int incx, incy, inc1, inc2;
-    int x, y;
-    dx = x2 - x1;
-    dy = y2 - y1;
-    if (dx < 0)
-        dx = -dx;
-    if (dy < 0)
-        dy = -dy;
-    incx = 1;
-    if (x2 < x1)
-        incx = -1;
-    incy = 1;
-    if (y2 < y1)
-        incy = -1;
-    x = x1;
-    y = y1;
-    if (dx > dy)
+    int x = 0, y = r;
+    int d = 3 - 2 * r;
+    while (y >= x)
     {
-        draw_pixel(x, y);
-        e = 2 * dy - dx;
-        inc1 = 2 * (dy - dx);
-        inc2 = 2 * dy;
-        for (i = 0; i < dx; i++)
+        plot(x, y, xc, yc);
+        x++;
+        if (d > 0)
         {
-            if (e >= 0)
-            {
-                y += incy;
-                e += inc1;
-            }
-            else
-            {
-                e += inc2;
-                x += incx;
-                draw_pixel(x, y);
-            }
+            y--;
+            d = d + 4 * (x - y) + 10;
         }
-    }
-    else
-    {
-        draw_pixel(x, y);
-        e = 2 * dx - dy;
-        inc1 = 2 * (dx - dy);
-        inc2 = 2 * dx;
-        for (i = 0; i < dy; i++)
+        else
         {
-            if (e >= 0)
-            {
-                x += incx;
-                e += inc1;
-            }
-            else
-            {
-                e += inc2;
-                y += incy;
-                draw_pixel(x, y);
-            }
+            d = d + 4 * x + 6;
         }
+        plot(x, y, xc, yc);
     }
 }
 void display()
 {
-    draw_line(x1, x2, y1, y2);
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(200, 300);
-    glVertex2i(120, 300);
-    glVertex2i(120, 50);
-    glVertex2i(200, 50);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(120, 300);
-    glVertex2i(160, 400);
-    glVertex2i(200, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(160, 400);
-    glVertex2i(380, 400);
-    glVertex2i(430, 300);
-    glVertex2i(200, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(430, 300);
-    glVertex2i(430, 50);
-    glVertex2i(120, 50);
-    glVertex2i(120, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(130, 50);
-    glVertex2i(130, 150);
-    glVertex2i(180, 150);
-    glVertex2i(180, 50);
-    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT);
+    bresenhamCircle(150, 350, 50);
+    bresenhamCircle(210, 350, 50);
+    bresenhamCircle(270, 350, 50);
+    bresenhamCircle(330, 350, 50);
     glFlush();
 }
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(200, 200);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Bresenham Line (house)");
-    myInit();
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Bresenham's Circle Drawing (Audi Symbol) ");
+        init();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
--- Bresenham doctor.cpp
+++ Bresenham doctor.cpp
@@ -1,125 +1,130 @@
-// Bresenham house
-#include <GL/glut.h>
-int x1, x2, y1, y2;
-void myInit()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0, 500, 0, 500);
+#include<GL/glut.h>
+using namespace std;
+
+void init(){
+    gluOrtho2D(-500 , 500 , -500 ,500);
 }
-void draw_pixel(int x, int y)
-{
+
+
+void draw_pixel(int x , int y){
     glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
-}
-void draw_line(int x1, int x2, int y1, int y2)
-{
-    int dx, dy, i, e;
-    int incx, incy, inc1, inc2;
-    int x, y;
-    dx = x2 - x1;
-    dy = y2 - y1;
-    if (dx < 0)
-        dx = -dx;
-    if (dy < 0)
-        dy = -dy;
-    incx = 1;
-    if (x2 < x1)
-        incx = -1;
-    incy = 1;
-    if (y2 < y1)
-        incy = -1;
-    x = x1;
-    y = y1;
-    if (dx > dy)
-    {
-        draw_pixel(x, y);
-        e = 2 * dy - dx;
-        inc1 = 2 * (dy - dx);
-        inc2 = 2 * dy;
-        for (i = 0; i < dx; i++)
-        {
-            if (e >= 0)
-            {
-                y += incy;
-                e += inc1;
-            }
-            else
-            {
-                e += inc2;
-                x += incx;
-                draw_pixel(x, y);
-            }
-        }
-    }
-    else
-    {
-        draw_pixel(x, y);
-        e = 2 * dx - dy;
-        inc1 = 2 * (dx - dy);
-        inc2 = 2 * dx;
-        for (i = 0; i < dy; i++)
-        {
-            if (e >= 0)
-            {
-                x += incx;
-                e += inc1;
-            }
-            else
-            {
-                e += inc2;
-                y += incy;
-                draw_pixel(x, y);
-            }
-        }
-    }
-}
-void display()
-{
-    draw_line(x1, x2, y1, y2);
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(200, 300);
-    glVertex2i(120, 300);
-    glVertex2i(120, 50);
-    glVertex2i(200, 50);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(120, 300);
-    glVertex2i(160, 400);
-    glVertex2i(200, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(160, 400);
-    glVertex2i(380, 400);
-    glVertex2i(430, 300);
-    glVertex2i(200, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(430, 300);
-    glVertex2i(430, 50);
-    glVertex2i(120, 50);
-    glVertex2i(120, 300);
-    glEnd();
-    glBegin(GL_LINE_LOOP);
-    glVertex2i(130, 50);
-    glVertex2i(130, 150);
-    glVertex2i(180, 150);
-    glVertex2i(180, 50);
+        glVertex2i(x , y);
     glEnd();
     glFlush();
 }
-int main(int argc, char **argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(200, 200);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Bresenham Line (house)");
-    myInit();
+
+void draw_line(int x1 , int y1 , int x2 ,int y2){
+
+    int dx , dy , p , x,y;
+    int incx ,incy,inc1,inc2;
+
+    dx = x2 - x1;
+    dy = y2 -y1;
+
+    if(dx <0) dx = -dx;
+    if(dy <0) dy = -dy;
+
+
+    incx = (x2 > x1) ? 1 : -1;
+    incy = (y2 > y1) ? 1 : -1;
+
+    x = x1;
+    y = y1;
+
+    if(dx > dy){
+
+        draw_pixel(x , y);
+        p = 2*dy - dx;
+
+        for(int i =0 ; i < dx ; i++){
+            if(p >= 0){
+                y += incy;
+                p += 2*(dy-dx);
+            }
+            else{
+                p += 2*dy;
+            }
+            x += incx;
+            draw_pixel(x , y);
+        }
+    }
+    else{
+
+        draw_pixel(x , y);
+        p = 2*dx - dy;
+
+        for(int i =0 ; i <dy ; i++){
+            if(p >= 0){
+                x += incx;
+                y += incy;
+                p += 2*(dy-dx);
+            }
+            else{
+                y += incy;
+                p += 2*dx;
+            }
+            draw_pixel(x , y);
+        }
+    }
+}
+
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //draw_line(50,50,200,150);
+
+    // Square
+    //draw_line(100, 100, 200, 100);
+    //draw_line(200, 100, 200, 200);
+    //draw_line(200, 200, 100, 200);
+    //draw_line(100, 200, 100, 100);
+
+    // Doctor
+    draw_line(-100 , -300 , 100 , -300);
+    draw_line(100 , -300 , 100 , -100);
+    draw_line(100 , -100 , 300 , -100);
+    draw_line(300 , -100 , 300 , 100);
+    draw_line(300 , 100 , 100 , 100);
+    draw_line(100 , 100 , 100 , 300);
+    draw_line(100 , 300 , -100 , 300);
+    draw_line(-100 , 300 , -100 , 100);
+    draw_line(-100 , 100 , -300 , 100);
+    draw_line(-300 , 100 , -300 , -100);
+    draw_line(-300 , -100 , -100 , -100);
+    draw_line(-100 , -100 , -100 , -300);
+}
+
+int main(int argc , char ** argv){
+
+    glutInit(&argc , argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Window");
+    init();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
